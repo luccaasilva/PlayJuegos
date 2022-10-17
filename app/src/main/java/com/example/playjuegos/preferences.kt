70a29dc3edff8488e4e26d375fd5eff1134b692c
luccaclase <aluara1699@ieselcaminas.org>
2022-10-17 20:54:28 +0200

    preferences

--- app/src/main/java/com/example/playjuegos/preferences.kt
+++ app/src/main/java/com/example/playjuegos/preferences.kt
@@ -3,24 +3,33 @@ package com.example.playjuegos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.core.view.get
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class preferences : AppCompatActivity() {
-    var rb=findViewById<RadioGroup>(R.id.radioGroup)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
         val fabButton = findViewById(R.id.fab0) as FloatingActionButton
+        var radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        var checked = radioGroup.checkedRadioButtonId
+        var juegoSeleccionado = findViewById<RadioButton>(checked)
         fabButton.setOnClickListener {
-            Toast.makeText(applicationContext, "No has pulsado ninguna opcion", Toast.LENGTH_LONG)
-                .show()
+            if (juegoSeleccionado.id == -1)
+                Toast.makeText(
+                    applicationContext,
+                    "Selecciona un Juego",
+                    Toast.LENGTH_LONG
+                ).show()
+            else if()
+                Toast.makeText(applicationContext, "${juegoSeleccionado.text}", Toast.LENGTH_LONG)
+                    .show()
         }
-        val radioGroup = findViewById(R.id.radioGroup) as RadioGroup
-        radioGroup.setOnClickListener {
-//            juegoSelecionado = radioGroup.
-        }
+
     }
 }
