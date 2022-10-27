e44e16e5230c7049ced2980ad12dd66929973b2e
luccaclase <aluara1699@ieselcaminas.org>
2022-10-27 16:51:40 +0200

    3

--- app/src/main/java/com/example/playjuegos/NewPlayer.kt
+++ app/src/main/java/com/example/playjuegos/NewPlayer.kt
@@ -9,26 +9,25 @@ class NewPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_player)
-        val telefonos = arrayOf("Scarface", "Titanic", "Pretty Woman", "Star Wars", "El rey León", "E.T")
+        val telefonos =
+            arrayOf("Telefono 1", "Telefono 2", "Telefono 3", "Telefono 4", "Telefono 5", "Telefono 6")
 
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, telefonos)
 
         val lista_telefonos = findViewById<Spinner>(R.id.spinner)
-        lista_telefonos.adapter=adaptador
+        lista_telefonos.adapter = adaptador
         adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         val telefono = findViewById<EditText>(R.id.edText4)
         lista_telefonos.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                telefono.hint = parent!!.getItemAtPosition(position).toString()
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
         })
     }
-
-
-
-
-
 
 
 }
---
+++ app/src/main/java/com/example/playjuegos/generos.kt
@@ -0,0 +1,11 @@
+package com.example.playjuegos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class generos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generos)
+    }
+}
